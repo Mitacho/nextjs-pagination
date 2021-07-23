ec08bf9c32b2960d6f13110fda3d3c1e9bdf2cfc
Mitacho <arthurf@tuta.io>
2021-07-23 00:13:30 +0000

    Deploying to gh-pages from @ Mitacho/nextjs-pagination@8feeefa9d979609320fbcf3218ac1d7d37a719ed 🚀

--- _next/static/H26EDdkDCxBjKYtO4Plch/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/789-f8d2c5361bb70de0be5f.js","static/chunks/pages/index-dea1836c5951b8bbecae.js"],"/_error":["static/chunks/pages/_error-157366ffea25803f02fd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
